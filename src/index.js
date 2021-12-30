57f755d070d8fea4c9a2a19b1504d9455b7fc180
heejun <hejeun22@gmail.com>
2021-12-30 17:47:29 +0900

    State Life Cycle

--- src/index.js
+++ src/index.js
@@ -4,64 +4,55 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>
-};
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  //생명주기 메서드
+  componentDidMount() {
+    this.timerID = setInterval(
+      () =>  this.tick()
+      , 1000);
+  };
+
+  //생명주기 메서드  
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, Render State world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>      
+    )
+  }
+}
 
 function App() {
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      <Clock />
+      <Clock />
+      <Clock />
     </div>
-  );
-};
-
-function Comment(props) {
-  return (
-    <div className='Comment'>
-      <UserInfo user={props.author} />
-      <div className='Comment-text'>
-        {props.text}
-      </div>
-      <div className='Comment-date'>
-        {/* {formatDate(props.date)} */}
-      </div>
-    </div>
-  );
-}
-
-
-//추출
-function Avatar(props) {
-  return (
-    <img className='Avatar' 
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
   )
 }
 
-function UserInfo(props) {
-  return (
-    <div className='UserInfo'>
-      <Avatar user={props.user} />
-      <div className='UserInfo-name'>
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-
-
-
 ReactDOM.render(
-  <App />, 
+  <App/>,
   document.getElementById('root')
 );
-
 
 
 // If you want to start measuring performance in your app, pass a function
