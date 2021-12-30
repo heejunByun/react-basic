7e7207a0f8035efdcdf6922a95e3b0426e0a9208
heejun <hejeun22@gmail.com>
2021-12-30 14:56:20 +0900

    element 소개

--- src/index.js
+++ src/index.js
@@ -4,23 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user) {
-  return user.firstName + '  ' + user.lastName
-}
 
-const user = {
-  firstName: 'Heejun',
-  lastName: 'Byun'
+function tick() {
+  const element = (
+    <div>
+      <h1>Clock</h1>
+      <h2>It is {new Date().toLocaleDateString()} {new Date().toLocaleTimeString()}. </h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
 };
 
-const element = <h1>Hello, {formatName(user)}</h1>;
+setInterval(tick, 1000);
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
