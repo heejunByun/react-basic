a88f541051e0969120c84879db0787faa74cbc24
heejun <hejeun22@gmail.com>
2021-12-31 16:14:01 +0900

    react 7. 조건부 렌더링

--- src/index.js
+++ src/index.js
@@ -3,33 +3,80 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
+// Greeting
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
 
-    // ????? 
-    this.handelClick = this.handelClick.bind(this); 
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+// Login
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props); //?
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  } 
+
+  handleLoginClick() {
+    console.log("this 11" , this);
+    this.setState({isLoggedIn: true});
   }
 
-  handelClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
+  handleLogoutClick() {
+    console.log("this 22 " , this);
+    this.setState({isLoggedIn: false});
   }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
     return (
-      <button onClick={this.handelClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
     );
+
   }
 }
 
 
 ReactDOM.render(
-  <Toggle />,
+  <LoginControl />,
   document.getElementById('root')
 );
 
